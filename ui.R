8f3ba8d9e65a853d47d1c0815adc680568b1e0b3
mchenini <amchenini@gmail.com>
2018-01-19 08:20:13 -0500

    update to ui.R

--- ui.R
+++ ui.R
@@ -2,6 +2,17 @@
 fluidPage(
   
   titlePanel("Movies explorer"),
+
+    comment <- renderPrint({ "On the left panel from the drop down menu titled “Which genre” you can select a movie genre (category) for instance “Action”" }),
+     comment(),
+     comment <- renderPrint({ "Then from the bottom drop down menu titled “Pick a movie”, you select a movie genre (category) for instance “Action”" }),
+     comment(),
+     comment <- renderPrint({ "The “Redraw graph” button, from the left panel, draws a curve under the “Budgets overtime” tab" }),
+     comment(),
+     comment <- renderPrint({ "The slider named “Year” on the left panel adjust the time range of the curve on the right panel" }),
+     comment(),
+     comment <- renderPrint({ "The “Movie picker” tab on right panel gives information about a specific movie." }),
+     comment(),
   
   sidebarLayout(
     sidebarPanel(
